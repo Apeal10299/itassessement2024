70c17bb45c9c7c2c41afc0d80d13b1a027ca9928
Apeal10299 <154609362+Apeal10299@users.noreply.github.com>
2024-04-12 23:18:31 +0545

    Html Project

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+
+    function changeLanguage(language) {
+        var element = document.getElementById("url");
+        element.value = language;
+        element.innerHTML = language;
+    }
+
+    function showDropdown() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
